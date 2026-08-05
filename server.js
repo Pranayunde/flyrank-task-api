2e54509508b3c7611c9d660f135053d5c8575193
Pranay Unde <pranayunde165@gmail.com>
2026-08-05 22:02:22 +0530

    Stage 0: setup server and supabase client

--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerSpec = require("./swagger");
 const taskRoutes = require("./routes/taskRoutes");
 const db = require("./db");
+const supabase = require("./config/supabase");
+require("dotenv").config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Middleware
 app.use(express.json());
@@ -19,4 +21,5 @@ app.use("/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 // Start Server
 app.listen(PORT, () => {
   console.log(`🚀 Server running at http://localhost:${PORT}`);
+  console.log("✅ Connected to Supabase");
 });
